816b9380949ca46a3f8167978f80b2e31894cf9d
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-09-28 17:50:02 +0545

    securing user password using Bcrypt

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,36 +1,49 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs"); // bcrypt is used for hashing passwords
 
-const home = async (req, res) =>{
+
+
+
+const home = async (req, res) => {
     try {
         res
-        .status(200)
-        .send('server is started using router');
+            .status(200)
+            .send('server is started using router');
     } catch (error) {
         console.log(error);
     }
 };
 
- //regesteration logic 
+//regesteration logic 
 
-const register = async (req, res) =>{
+const register = async (req, res) => {
     try {
         console.log(req.body);
-        const {username, email, phone, password } = req.body;
+        const { username, email, phone, password } = req.body;
 
-        const userExist = await User.findOne({email}); //checking the existence of the email
+        const userExist = await User.findOne({ email }); //checking the existence of the email
 
-        if(userExist){
-            return res.status(400).json({msg: 'User already exists. Try logging in insted'});
+        if (userExist) {
+            return res.status(400).json({ msg: 'User already exists. Try logging in insted' });
         }
 
-        const userCreated = await User.create({username, email, phone, password });
+        // hashing password
+        // const saltRound = 10;
+        // const hash_passsword = await bcrypt.hash(password, saltRound);
+
+        const userCreated = await User.create({ username,
+            email,
+            phone,
+            password,
+            // password: hash_passsword,
+         });
 
 
-        res.status(200).json({ msg: 'User created successfully'});
+        res.status(201).json({ msg: userCreated });
     } catch (error) {
-        res.status(400).send({msg: "page not found"})
+        res.status(400).send({ msg: "page not found" })
     }
-    
+
 };
 
 module.exports = { home, register };
--- models/user-model.js
+++ models/user-model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+
 
 //adding userschema
-
 const userSchema = new mongoose.Schema({
     username: {
         type: 'string',
@@ -19,14 +20,31 @@ const userSchema = new mongoose.Schema({
         type: 'string',
         require: true,
     },
-    isAdmin:{
+    isAdmin: {
         type: 'boolean',
         default: false, //defalut false because we donot create everyone admin 
     }
 });
 
+//? secure password with the bcrypt
+userSchema.pre('save', async function(next){//data save hunu vanda aagadi ya store vako data haru suru ma execute hunxa
+    console.log("pre method",this);
+    const user = this;
+
+    if(!user.isModified("password")){ 
+        next();
+    }
+
+    try {
+        const saltRound = await bcrypt.genSalt(10);
+        const hash_passsword = await bcrypt.hash(user.password, saltRound); 
+        user.password = hash_passsword;
+    } catch (error) {
+      next(error);  
+    }
+})
+
+
 //defineing model/collection name
-
 const User = new mongoose.model("User", userSchema);
-
 module.exports = User;
