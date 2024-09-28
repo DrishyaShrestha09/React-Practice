56764d04efd9dea2b4e50c1c8f467230e284cecf
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-09-28 23:58:33 +0545

    Built contact form with some validations

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const router = require("./router/auth-router");
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
@@ -9,7 +10,8 @@ const errorMiddleware = require("./middlewares/error-middleware");
 app.use(express.json()); // this means that in this app now we can use json. This middleware is responsible for parsing JSON data from the requests and it should be applied at the beginning of the middleware
 
 //connecting to the databse 
-app.use("/api/auth", router);
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
 
 app.use(errorMiddleware);
 
