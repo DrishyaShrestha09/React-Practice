05518cfea5ff9f9476d557666fef84879b24e14a
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-09-28 13:58:33 +0545

    added user regesteration and verified using postman

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const router = require("./router/auth-router");
 
+
+// middleware
+app.use(express.json()); // this means that in this app now we can use json. This middleware is responsible for parsing JSON data from the requests and it should be applied at the beginning of the middleware
+
+
 app.use("/api/auth", router);
 
 
