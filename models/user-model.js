9886c48fa89d1e10ca4c935605e87daae1472db4
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-09-28 16:36:39 +0545

    created user schema and model for user regestration

---
+++ models/user-model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+//adding userschema
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: 'string',
+        require: true,
+    },
+    email: {
+        type: 'string',
+        require: true,
+    },
+    phone: {
+        type: 'string',
+        require: true,
+    },
+    password: {
+        type: 'string',
+        require: true,
+    },
+    isAdmin:{
+        type: 'boolean',
+        default: false, //defalut false because we donot create everyone admin 
+    }
+});
+
+//defineing model/collection name
+
+const User = new mongoose.model("User", userSchema);
+
+moodule.exports = User;
