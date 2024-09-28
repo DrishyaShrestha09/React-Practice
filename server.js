5203e323f1bcee429a2d2a33e045b3c2c0e4f726
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-09-28 14:20:12 +0545

    Connected to the databse

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const router = require("./router/auth-router");
-
+const connectDb = require("./utils/db");
 
 // middleware
 app.use(express.json()); // this means that in this app now we can use json. This middleware is responsible for parsing JSON data from the requests and it should be applied at the beginning of the middleware
 
-
+//connecting to the databse 
 app.use("/api/auth", router);
 
 
 const PORT = 5000;
-app.listen(PORT, () =>{
-    console.log(`server is running at port: ${PORT}`);
+
+connectDb().then(() => {
+    app.listen(PORT, () => {
+        console.log(`server is running at port: ${PORT}`);
+    });
 });
